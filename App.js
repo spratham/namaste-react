028ce340b36cff3ca694aaa0709788e27db5d16b
Pratham <pratham.s@crowdanalytix.com>
2024-04-05 23:14:48 +0530

    episode-2-parcel-and-build

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("div", { id: "heading" }, [
-  React.createElement("div", { id: "topic" }, "nested"),
+  React.createElement("div", { id: "topic" }, "blah bleh "),
   React.createElement("div", { id: "h" }, "div through react object"),
 ]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
