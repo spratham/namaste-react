6b6fa3745c5e5a517b4d6dd79cc94dc26d3ffe55
Pratham <pratham.s@crowdanalytix.com>
2024-04-18 20:50:42 +0530

    Searching through names

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,23 +3,37 @@ import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
 
 export default BodyComponent = () => {
-  // Local State Variable - Super powerful variable
+  // State variable to hold the list of restaurants
   const [listOfRestaurant, setListOfRestaurant] = useState(resList);
 
+  // Function to handle search input change
+  const handleSearchChange = (event) => {
+    const searchText = event.target.value.toLowerCase();
+    const searchedList = listOfRestaurant.filter((res) =>
+      res.info.name.toLowerCase().includes(searchText)
+    );
+    setListOfRestaurant(searchText.length ? searchedList : resList); //Render whole list after clearing the search
+  };
   return (
     <div className="body-container">
       <div className="search">
-        <input placeholder="Search your favoraite restaurant..."></input>
+        <input
+          placeholder="Search your favorite restaurant..."
+          onChange={handleSearchChange}
+        />
+        <button type="submit" className="filter-btn">
+          Search
+        </button>
       </div>
       <div className="filter">
         <button
           onClick={() => {
             //filter Logic
-            const filteredist = resList.filter(
-              (res) => res.info.avgRating > 4.4
+            const filteredList = resList.filter(
+              (res) => res.info.avgRating > 4
             );
-            setListOfRestaurant(filteredist);
-            console.log(filteredist);
+            setListOfRestaurant(filteredList);
+            console.log(filteredList);
           }}
           className="filter-btn"
         >
